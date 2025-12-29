7f6117610299118546fff2731de9d1d9694e99eb
VuaTroApp <appvuatrovn@gmail.com>
2025-12-29 14:58:30 +0700

    test with project

---
+++ src/main/java/com/example/VuaTroVN_BE/VuaTroVnBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.VuaTroVN_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuaTroVnBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuaTroVnBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VuaTroVN_BE/VuaTroVnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VuaTroVN_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuaTroVnBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
