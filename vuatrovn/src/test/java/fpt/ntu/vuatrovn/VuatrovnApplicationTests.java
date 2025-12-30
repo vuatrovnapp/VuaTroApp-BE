e0079834ce8a9a2282ba3367c677d7563802643f
Nguyễn Bùi Quang Huy <2k4quanghuy@gmail.com>
2025-12-30 13:17:25 +0700

    Khởi tạo dự án Spring Boot

---
+++ vuatrovn/src/test/java/fpt/ntu/vuatrovn/VuatrovnApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.ntu.vuatrovn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuatrovnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
