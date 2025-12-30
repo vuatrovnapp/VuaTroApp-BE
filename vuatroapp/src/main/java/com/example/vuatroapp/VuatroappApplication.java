4a230a2e360b691e5ae5213e5e859b14f1f40264
Fyng217 <vodt7562@gmail.com>
2025-12-30 15:14:59 +0700

    Khởi tạo dự án Spring Boot

---
+++ vuatroapp/src/main/java/com/example/vuatroapp/VuatroappApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuatroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuatroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuatroappApplication.class, args);
+	}
+
+}
